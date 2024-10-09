1d8a929c0e77e6fa2856c0b0903d499f5704dddd
Leon <junleon.cha@gmail.com>
2024-10-09 13:22:00 +0200

    add activity

--- lib/services/course/activity.dart
+++ lib/services/course/activity.dart
@@ -1,28 +1,41 @@
+import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class ActivityServices {
+class ActivityServices extends GetxController {
   final SupabaseClient supabase = Supabase.instance.client;
 
-  getCurrentUserActivityDetails(String id) async {
-    final activity = await supabase
-        .from("activities")
-        .select("*")
-        .eq("profil_id", id)
-        .single();
-    return activity;
+  // Observable pour stocker les détails de l'activité
+  var activityDetail = {}.obs;
+
+  Future<List<Map<String, dynamic>>> fetchCurrentUserActivityDetails(
+      String id) async {
+    try {
+      final List<dynamic> activities =
+          await supabase.from("activities").select("*").eq("profile_id", id);
+      print(activities);
+      // Convertir les résultats en List<Map<String, dynamic>>
+      return activities
+          .map((activity) => activity as Map<String, dynamic>)
+          .toList();
+    } catch (e) {
+      print('Error fetching activity details: $e');
+      return []; // Retourne une liste vide en cas d'erreur
+    }
   }
 
   Future<dynamic> getCurrentUserLastActivityDetails(String id) async {
-    final lastActivity = await supabase
-        .from("activities")
-        .select("*")
-        .eq("profile_id", id)
-        .order("created_at", ascending: false)
-        .limit(1)
-        .single();
-
-    print(lastActivity);
-
-    return lastActivity;
+    try {
+      final lastActivity = await supabase
+          .from("activities")
+          .select("*")
+          .eq("profile_id", id)
+          .order("created_at", ascending: false)
+          .limit(1)
+          .single();
+      return lastActivity;
+    } catch (e) {
+      print('Error fetching last activity details: $e');
+      return null;
+    }
   }
 }
