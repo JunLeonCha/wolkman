cbee4f58133bf6a02c9b57f06294473aa81be820
Leon <junleon.cha@gmail.com>
2024-10-09 15:26:52 +0200

    update followup sheet

--- lib/components/follow_up_sheet.dart
+++ lib/components/follow_up_sheet.dart
@@ -5,29 +5,65 @@ import 'package:wolkman/views/list-activities.dart';
 
 class FollowUpSheet extends StatelessWidget {
   final String? userId;
+  final ScrollController _scrollController = ScrollController();
 
-  const FollowUpSheet({super.key, required this.userId});
+  FollowUpSheet({super.key, required this.userId});
+
+  void scrollLeft() {
+    _scrollController.animateTo(
+      _scrollController.position.pixels - 100,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void scrollRight() {
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 100,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Row(
-          children: [
-            ElevatedButton(onPressed: () => {}, child: const Text("Moi")),
-            const Gap(20),
-            ElevatedButton(onPressed: () => {}, child: const Text("Suivi")),
-            const Gap(20),
-            ElevatedButton(
-                onPressed: () => {Get.to(ListActivities(userId: userId))},
-                child: const Text("Liste des activitées")),
-            const Gap(20),
-            ElevatedButton(
-                onPressed: () => {Get.toNamed("/map")},
-                child: const Text("Map")),
-          ],
-        ),
-      ],
+    return Container(
+      child: Row(
+        children: [
+          IconButton(
+            icon: const Icon(Icons.arrow_left),
+            color: Colors.cyan[300],
+            onPressed: scrollLeft,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              controller: _scrollController,
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ElevatedButton(onPressed: () => {}, child: const Text("Moi")),
+                  const Gap(20),
+                  ElevatedButton(
+                      onPressed: () => {}, child: const Text("Suivi")),
+                  const Gap(20),
+                  ElevatedButton(
+                      onPressed: () => {Get.to(ListActivities(userId: userId))},
+                      child: const Text("Liste des activitées")),
+                  const Gap(20),
+                  ElevatedButton(
+                      onPressed: () => {Get.toNamed("/map")},
+                      child: const Text("Map")),
+                ],
+              ),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.arrow_right),
+            color: Colors.cyan[300],
+            onPressed: scrollRight,
+          ),
+        ],
+      ),
     );
   }
 }
